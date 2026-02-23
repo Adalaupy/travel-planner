c761859bee98efe119f5a0ca2a899d26e6fc9a76
Ada Lau <adalau@sino.com>
2026-02-23 15:21:42 +0800

    Remove build output from version control

--- out/_next/static/chunks/29c592db770f33e2.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/2989c803855935e3.js",
-  "static/chunks/b409d42c8348c6d2.js",
-  "static/chunks/3833322252b952de.js",
-  "static/chunks/a8c6797706263037.js",
-  "static/chunks/04b945fe68aac9b2.css",
-  "static/chunks/turbopack-7e96acd72bcfd3e7.js"
-])
--- out/_next/static/chunks/b1ef2575eb91d6c5.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/my-trips", [
-  "static/chunks/152914bbb0855388.js",
-  "static/chunks/b409d42c8348c6d2.js",
-  "static/chunks/a8c6797706263037.js",
-  "static/chunks/3833322252b952de.js",
-  "static/chunks/9cf3fabbc2914573.js",
-  "static/chunks/65b4ac08490b4fb2.css",
-  "static/chunks/turbopack-08e8fdfc65dc624c.js"
-])
