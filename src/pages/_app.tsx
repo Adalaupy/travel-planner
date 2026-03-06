7ff699529dadb3febe3100701cbdfc7accb003a7
Ada Lau <adalau@sino.com>
2026-03-06 15:49:12 +0800

    update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 ﻿import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 import '../styles/components.module.css'
 import Header from '../components/Header'
@@ -8,6 +9,9 @@ import UsernameModal from '../components/UsernameModal'
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <UsernameProvider>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <UsernameModal />
             <Header />
             <Component {...pageProps} />
